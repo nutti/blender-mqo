cc7b1cbf1ea8e91b5e9e8175256810042193527d
nutti <nutti.metro@gmail.com>
2024-09-08 20:48:08 +0900

    WIP

--- src/blender_mqo/__init__.py
+++ src/blender_mqo/__init__.py
@@ -1,10 +1,10 @@
 bl_info = {
-    "name": "MQO (Metasequoia) format",
+    "name": "MQO (Metasequoia) Format File Importer/Exporter",
     "author": "nutti, sapper-trle",
-    "version": (1, 4, 0),
-    "blender": (2, 80, 0),
+    "version": (1, 5, 0),
+    "blender": (4, 2, 0),
     "location": "",
-    "description": "Import/Export MQO files",
+    "description": "Import/Export MQO format files",
     "warning": "",
     "support": "COMMUNITY",
     "wiki_url": "",
@@ -26,26 +26,8 @@ else:
     from . import preferences
     from . import ops
 
-# pylint: disable=C0413
-import os
-
-
-def register_updater(info):
-    config = utils.addon_updater.AddonUpdatorConfig()
-    config.owner = "nutti"
-    config.repository = "blender-mqo"
-    config.current_addon_path = os.path.dirname(os.path.realpath(__file__))
-    config.branches = ["master", "develop"]
-    ridx = config.current_addon_path.rfind(utils.addon_updater.get_separator())
-    config.addon_directory = config.current_addon_path[:ridx]
-    config.min_release_version = info["version"]
-    config.target_addon_path = "src/blender_mqo"
-    updater = utils.addon_updater.AddonUpdatorManager.get_instance()
-    updater.init(config)
-
 
 def register():
-    register_updater(bl_info)
     bpy.utils.register_class(ops.BLMQO_ObjectImportPropertyCollection)
     bpy.utils.register_class(ops.BLMQO_MaterialImportPropertyCollection)
     bpy.utils.register_class(ops.BLMQO_ObjectExportPropertyCollection)
@@ -53,24 +35,16 @@ def register():
     bpy.utils.register_class(ops.BLMQO_VertexWeightExportPropertyCollection)
     utils.bl_class_registry.BlClassRegistry.register()
 
-    if utils.compatibility.check_version(2, 80, 0) >= 0:
-        file_import_menu_type = bpy.types.TOPBAR_MT_file_import
-        file_export_menu_type = bpy.types.TOPBAR_MT_file_export
-    else:
-        file_import_menu_type = bpy.types.INFO_MT_file_import
-        file_export_menu_type = bpy.types.INFO_MT_file_export
+    file_import_menu_type = bpy.types.TOPBAR_MT_file_import
+    file_export_menu_type = bpy.types.TOPBAR_MT_file_export
 
     file_import_menu_type.append(ops.topbar_mt_file_import_fn)
     file_export_menu_type.append(ops.topbar_mt_file_export_fn)
 
 
 def unregister():
-    if utils.compatibility.check_version(2, 80, 0) >= 0:
-        file_import_menu_type = bpy.types.TOPBAR_MT_file_import
-        file_export_menu_type = bpy.types.TOPBAR_MT_file_export
-    else:
-        file_import_menu_type = bpy.types.INFO_MT_file_import
-        file_export_menu_type = bpy.types.INFO_MT_file_export
+    file_import_menu_type = bpy.types.TOPBAR_MT_file_import
+    file_export_menu_type = bpy.types.TOPBAR_MT_file_export
 
     file_export_menu_type.remove(ops.topbar_mt_file_export_fn)
     file_import_menu_type.remove(ops.topbar_mt_file_import_fn)
--- src/blender_mqo/utils/__init__.py
+++ src/blender_mqo/utils/__init__.py
@@ -2,12 +2,10 @@ if "bpy" in locals():
     import importlib
     # pylint: disable=E0601
     importlib.reload(mqo_file)
-    importlib.reload(addon_updater)
     importlib.reload(bl_class_registry)
     importlib.reload(compatibility)
 else:
     from . import mqo_file
-    from . import addon_updater
     from . import bl_class_registry
     from . import compatibility
 
